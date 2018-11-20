39d8f9b95b33e1d55c9addf8c520e33e72943d33
NathanHodgkiss447 <33458168+NathanHodgkiss447@users.noreply.github.com>
2018-11-20 13:55:47 +0000

    Add files via upload

---
+++ presidentApp/src/presidentapp/candidate.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presidentapp;
+
+/**
+ *
+ * @author x17381176
+ */
+public class candidate {
+    //Data
+    private String name;
+    private int age;
+    private String sex;
+    private String party;
+    
+    
+}
