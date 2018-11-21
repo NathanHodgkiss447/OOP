cfa9fea426cf880cacfd6f14053d806aa267d933
NathanHodgkiss447 <33458168+NathanHodgkiss447@users.noreply.github.com>
2018-11-21 09:36:00 +0000

    Testing to see if GIT works

---
+++ src/presidentapp/gallagher.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presidentapp;
+
+/**
+ *
+ * @author x17381176
+ */
+public class gallagher extends candidate {
+    //Data
+    private String policy;
+    private String budget;
+    
+}
