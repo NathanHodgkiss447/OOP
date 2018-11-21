5a538572a2e4d307adb45bfb76a43093b80f4e50
dylanm29799 <33895740+dylanm29799@users.noreply.github.com>
2018-11-21 10:16:30 +0000

    hi3

--- src/presidentapp/candidate.java
+++ src/presidentapp/candidate.java
@@ -10,15 +10,50 @@ package presidentapp;
  * @author x17381176
  */
 public class candidate {
+
     //Data
     private String name;
     private int age;
     private String sex;
     private String party;
- 
-    public Student(){
-this.name="";
-this.age=0;
-this.grade=0.0;
-}
+
+    public candidate() {
+        this.name = "";
+        this.age = 0;
+        this.sex = "";
+        this.party = "";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
 }
