3bc3f12ab576626f3881ce6ebbcfa4664378c719
dylanm29799 <33895740+dylanm29799@users.noreply.github.com>
2018-11-21 10:24:17 +0000

    hi4

--- src/presidentapp/PresidentApp.java
+++ src/presidentapp/PresidentApp.java
@@ -16,7 +16,8 @@ public class PresidentApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        presidentGUI myGUI=new presidentGUI();
+        myGUI.setVisible(true);
         
         
     }
